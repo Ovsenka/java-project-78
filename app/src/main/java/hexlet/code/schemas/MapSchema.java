6da045ffd060474a7b7e2c814409f11aa5420f0e
Ovsenka <ovsenka3@gmail.com>
2024-10-17 23:49:14 +0300

    refactor

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,11 +1,9 @@
 package hexlet.code.schemas;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 public final class MapSchema<K, V> extends BaseSchema<Map<K, V>> {
-    private final Map<K, BaseSchema<V>> valueSchemas = new HashMap<>();
 
     public MapSchema<K, V> required() {
         addCheck("required", Objects::nonNull);
@@ -16,27 +14,14 @@ public final class MapSchema<K, V> extends BaseSchema<Map<K, V>> {
         addCheck("minLength", value -> value.size() == size);
         return this;
     }
-    public void shape(Map<K, BaseSchema<V>> schemas) {
-        this.valueSchemas.putAll(schemas);
-    }
-
-    @Override
-    public boolean isValid(Map<K, V> mapToValidate) {
-        boolean isValidMap = super.getChecks().entrySet()
-                .stream()
-                .allMatch(entry -> entry.getValue().test(mapToValidate));
-
-        boolean isValidMapValues = this.valueSchemas.entrySet()
-                .stream()
-                .allMatch((entry) -> {
-                    K key = entry.getKey();
-                    BaseSchema<V> schemaForValue = entry.getValue();
-                    if (!mapToValidate.containsKey(key)) {
-                        return false;
-                    } else {
-                        return schemaForValue.isValid(mapToValidate.get(key));
-                    }
-                });
-        return isValidMap && isValidMapValues;
+    public <T> MapSchema shape(Map<String, BaseSchema<T>> schemas) {
+        addCheck(
+                "shape",
+                map -> schemas.entrySet().stream().allMatch(e -> {
+                    V val = map.get(e.getKey());
+                    return e.getValue().isValid((T) val);
+                }
+                ));
+        return this;
     }
 }
